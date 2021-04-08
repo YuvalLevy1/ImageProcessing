0bc2c3bd9f4eaf940a8fae04c75d82314abbb169
YuvalLevy1 <yuval124levy1@gmail.com>
2021-04-08 15:28:14 +0300

    added util class for generic helper functions

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+def convert_rgb_hsv(rgb):
+    return cv2.cvtColor(np.uint8([[rgb]]), cv2.COLOR_RGB2HSV)
+
+
+def is_collided_with_surface(surface, coordinates):
+    return surface.get_rect().collidepoint(coordinates)
