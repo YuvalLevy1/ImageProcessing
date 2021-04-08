74e48392a17a905a2a88bb07d6c2be5cad8e6892
yuvalLevy1 <yuval124levy1@gmail.com>
2021-04-08 16:24:24 +0300

    made collision function to work by size and not pygame's collision function

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 
+import camera
+
 
 def convert_rgb_hsv(rgb):
     return cv2.cvtColor(np.uint8([[rgb]]), cv2.COLOR_RGB2HSV)
 
 
-def is_collided_with_surface(surface, coordinates):
-    return surface.get_rect().collidepoint(coordinates)
+def is_collided_with_camera(camera_coordinates, coordinates):
+    return camera_coordinates[0] <= coordinates[0] <= camera_coordinates[0] + camera.IMAGE_WIDTH \
+           and camera_coordinates[1] <= coordinates[1] <= camera_coordinates[1] + camera.IMAGE_LENGTH
