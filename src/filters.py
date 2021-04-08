5a8acdf9ccbafc236d0229dbb261cd924bc3de3f
YuvalLevy1 <yuval124levy1@gmail.com>
2021-04-08 15:30:28 +0300

    made it possible to move a filter's sliders according to hsv values

--- src/filters.py
+++ src/filters.py
@@ -9,12 +9,13 @@ class Filter:
         self.__x = x
         self.__y = y
         font = pygame.font.SysFont('Corbel', 30)
-        self.title = font.render(title, True, (0, 0, 0))
-        self.width = self.title.get_width()
-        self.height = self.title.get_height()
+        self.title = title
+        self.title_font = font.render(title, True, (0, 0, 0))
+        self.width = self.title_font.get_width()
+        self.height = self.title_font.get_height()
         self.sliders = []
         self.sliders_coordinates = [(self.__x + slider.VALUE_SPACE,
-                                     self.__y + self.title.get_height() + slider.SLIDER_HEIGHT * 2)]
+                                     self.__y + self.title_font.get_height() + slider.SLIDER_HEIGHT * 2)]
         self.slider_amount = 0
 
     def get_width(self):
@@ -22,15 +23,15 @@ class Filter:
         for slider in self.sliders:
             if slider.get_size() > max_width:
                 max_width = slider.get_size()
-        if max_width > self.title.get_width():
+        if max_width > self.title_font.get_width():
             return max_width
-        return self.title.get_width()
+        return self.title_font.get_width()
 
     def get_height(self):
-        return slider.SLIDER_HEIGHT * len(self.sliders) * 2 + self.title.get_height() + slider.SLIDER_HEIGHT
+        return slider.SLIDER_HEIGHT * len(self.sliders) * 2 + self.title_font.get_height() + slider.SLIDER_HEIGHT
 
     def get_title_coordinates(self):
-        return self.__x + self.width / 2 - self.title.get_width() / 2 - 15, self.__y
+        return self.__x + self.width / 2 - self.title_font.get_width() / 2 - 15, self.__y
 
     def __get_slider_coordinates(self):
         self.sliders_coordinates.append(
@@ -49,12 +50,25 @@ class Filter:
 
 
 class HSV_Filter(Filter):
-    def __init__(self, x, y, title):
-        super().__init__(x, y, title)
+    def __init__(self, x, y):
+        super().__init__(x, y, "hsv")
         self.add_slider(0, 255, 100, 't')
         self.add_slider(0, 255, 100, 'h')
         self.add_slider(0, 255, 100, 's')
         self.add_slider(0, 255, 100, 'v')
+
+    def __move_to_value(self, text, value):
+        for slider in self.sliders:
+            if slider.text == text:
+                slider.held = True
+                slider.move_circle(slider.get_circle_x_by_value(value))
+                slider.held = False
+
+    def change_hsv_values(self, hsv):
+        self.__move_to_value("h", hsv[0])
+        self.__move_to_value("s", hsv[1])
+        self.__move_to_value("v", hsv[2])
+        self.__move_to_value("t", 70)
 
     def get_value(self, text):
         for slider in self.sliders:
