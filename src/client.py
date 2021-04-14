0a8b49c66fd49c42f6fc6aada41aafa06733c780
YuvalLevy1 <yuval124levy1@gmail.com>
2021-04-14 16:02:59 +0300

    added center finding

--- src/client.py
+++ src/client.py
@@ -8,14 +8,17 @@ class Client:
         self.sender = imagezmq.ImageSender(connect_to="tcp://{}:{}".format(address, IP_port))
         self.camera = camera.Camera(camera_port)
 
-    def send_image(self):
-        self.sender.send_image("frame", self.camera.get_image_bgr())
+    def send_camera_image(self):
+        self.send_image("frame", self.camera.get_image_bgr())
+
+    def send_image(self, message, image):
+        self.sender.send_image(message, image)
 
 
 def main():
     client = Client("127.0.0.1", 5555, 0)
     while True:
-        client.send_image()
+        client.send_camera_image()
 
 
 if __name__ == '__main__':
